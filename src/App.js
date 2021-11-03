80459df5709a2700465b056b1af56f8c78cea0ef
ilkaay <ilkaay.yuksel@gmail.com>
2021-11-03 20:12:32 +0300

    fixed row and col of four bugs

--- src/App.js
+++ src/App.js
@@ -28,10 +28,12 @@ const App = () => {
     for (let i = 0; i <= 47; i++) {
       const columnOfThree = [i, i + width, i + width * 2];
       const decidedColor = currentColorArrangement[i];
+      const isBlank = currentColorArrangement[i] === blank;
 
       if (
         columnOfThree.every(
-          (square) => currentColorArrangement[square] === decidedColor
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
         )
       ) {
         columnOfThree.forEach(
@@ -46,6 +48,7 @@ const App = () => {
     for (let i = 0; i < 64; i++) {
       const rowOfThree = [i, i + 1, i + 2];
       const decidedColor = currentColorArrangement[i];
+      const isBlank = currentColorArrangement[i] === blank;
       const notValid = [
         6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
       ];
@@ -54,7 +57,8 @@ const App = () => {
 
       if (
         rowOfThree.every(
-          (square) => currentColorArrangement[square] === decidedColor
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
         )
       ) {
         rowOfThree.forEach(
@@ -69,10 +73,12 @@ const App = () => {
     for (let i = 0; i <= 39; i++) {
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
       const decidedColor = currentColorArrangement[i];
+      const isBlank = currentColorArrangement[i] === blank;
 
       if (
         columnOfFour.every(
-          (square) => currentColorArrangement[square] === decidedColor
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
         )
       ) {
         columnOfFour.forEach(
@@ -91,12 +97,14 @@ const App = () => {
         5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
         54, 55, 62, 63, 64,
       ];
+      const isBlank = currentColorArrangement[i] === blank;
 
       if (notValid.includes(i)) continue;
 
       if (
         rowOfFour.every(
-          (square) => currentColorArrangement[square] === decidedColor
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
         )
       ) {
         rowOfFour.forEach(
@@ -145,6 +153,7 @@ const App = () => {
       squareBeingDragged.getAttribute("src");
     currentColorArrangement[squareBeingDraggedId] =
       squareBeingReplaced.getAttribute("src");
+
     const validMoves = [
       squareBeingDraggedId - 1,
       squareBeingDraggedId - width,
@@ -154,13 +163,15 @@ const App = () => {
 
     const validMove = validMoves.includes(squareBeingReplacedId);
 
+    const isAColumnOfFour = checkForColumnOfFour();
+    const isARowOfFour = checkForRowOfFour();
+    const isAColumnOfThree = checkForColumnOfThree();
+    const isARowOfThree = checkForRowOfThree();
+
     if (
       squareBeingReplacedId &&
       validMove &&
-      (checkForRowOfThree() ||
-        checkForColumnOfThree() ||
-        checkForRowOfFour() ||
-        checkForColumnOfFour())
+      (isARowOfThree || isARowOfFour || isAColumnOfFour || isAColumnOfThree)
     ) {
       setSquareBeingDragged(null);
       setSquareBeingReplaced(null);
@@ -210,7 +221,7 @@ const App = () => {
   ]);
 
   return (
-    <div className='app'>
+    <div className='app justify-content-center'>
       <div className='game'>
         {currentColorArrangement.map((candyColor, index) => (
           <img
